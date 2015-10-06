716f4c53bf71792426425bfe674a314ffc71a550
Ankit Bahuguna <ankit@cliqz.com>
2015-10-06 19:09:37 +0200

    Added preprocess_line() to helper_functions.py

--- modules/helper_functions.py
+++ modules/helper_functions.py
@@ -9,8 +9,21 @@ def computeCosineDistance(vector1, vector2):
 # Returns a Boolean: True if all items of the vetors are equal, false other wise
 def checkVectorEquality(array1, array2):
 	if (array1 == array2).all():
-		return true
+		return True
 	else:
-		return false
+		return False
 
 
+# Removes (,), ", ', ., "  ", "   " ,  „  and “
+
+def preprocess_line(line):
+    line_tmp1 = line.replace("(", " ")
+    line_tmp2 = line_tmp1.replace(")", " ")
+    line_tmp3 = line_tmp2.replace("\"", " ")
+    line_tmp4 = line_tmp3.replace("\'", " ")
+    line_tmp5 = line_tmp4.replace('„', " ")
+    line_tmp6 = line_tmp5.replace('“', " ")
+    line_tmp7 = line_tmp6.replace('.', " . ")
+    line_tmp8 = line_tmp7.replace('  ', " ")
+    line_tmp9 = line_tmp8.replace('   ', " ")
+    return line_tmp9
